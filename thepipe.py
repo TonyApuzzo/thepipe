ef4c23328f680dc9f0e0de0a145dd16b049109b5
Tony Apuzzo <tonyapuzzo@yahoo.com>
2024-04-18 13:14:36 -0600

    feat: add --output argument

--- thepipe.py
+++ thepipe.py
@@ -29,9 +29,9 @@ def create_messages_from_chunks(chunks: List[Chunk]) -> List[Dict]:
         messages.append({"role": "system", "content": content})
     return messages
 
-def save_outputs(chunks: List[Chunk], verbose: bool = False, text_only: bool = False) -> None:
-    if not os.path.exists('outputs'):
-        os.makedirs('outputs')
+def save_outputs(chunks: List[Chunk], verbose: bool = False, text_only: bool = False, output: str = 'outputs') -> None:
+    if not os.path.exists(output):
+        os.makedirs(output)
     # Save the chunks to files
     text = ""
     n_images = 0
@@ -43,12 +43,12 @@ def save_outputs(chunks: List[Chunk], verbose: bool = False, text_only: bool = F
         if chunk.image is not None:
             clean_path = chunk.path.replace('/', '_').replace('\\', '_')
             clean_path = re.sub(r"[^a-zA-Z0-9 _]", "", clean_path)
-            chunk.image.convert('RGB').save(f'outputs/{clean_path}_{i}.jpg')
+            chunk.image.convert('RGB').save(f'{output}/{clean_path}_{i}.jpg')
             n_images += 1
     # Save the text
-    with open(f'outputs/prompt.txt', 'w', encoding='utf-8') as file:
+    with open(f'{output}/prompt.txt', 'w', encoding='utf-8') as file:
         file.write(text)
-    if verbose: print_status(f"Output {len(text)/4} tokens and {n_images} images to 'outputs'", status='success')
+    if verbose: print_status(f"Output {len(text)/4} tokens and {n_images} images to '{output}'", status='success')
 
 def extract(source: str, match: Optional[str] = None, ignore: Optional[str] = None, limit: int = 1e5, verbose: bool = False, mathpix: bool = False, text_only: bool = False) -> List[Dict]:
     chunks = extractor.extract_from_source(source=source, match=match, ignore=ignore, limit=limit, mathpix=mathpix, text_only=text_only, verbose=verbose)
@@ -63,6 +63,7 @@ def parse_arguments() -> argparse.Namespace:
     parser.add_argument('--match', type=str, default=None, help='The glob filename pattern to match in the directory. Glob notation, not regex. Only matches filenames, not paths.')
     parser.add_argument('--ignore', type=str, default=None, help='The regex filepath pattern to ignore in the directory. Regex notation, not glob. Matches filenames and paths.')
     parser.add_argument('--limit', type=float, default=1e5, help='The token limit for the compressed project context.')
+    parser.add_argument('--output', type=str, default='outputs', help='The output folder.')
     parser.add_argument('--mathpix', action='store_true', help='Use Mathpix to extract text from images.')
     parser.add_argument('--text_only', action='store_true', help='Extract only text from the source.')
     parser.add_argument('--quiet', action='store_true', help='Do not print status messages.')
@@ -75,4 +76,4 @@ if __name__ == '__main__':
     args = parse_arguments()
     chunks = extractor.extract_from_source(source=args.source, match=args.match, ignore=args.ignore, limit=args.limit, mathpix=args.mathpix, text_only=args.text_only, verbose=args.verbose)
     chunks = compressor.compress_chunks(chunks=chunks, verbose=args.verbose, limit=args.limit)
-    save_outputs(chunks=chunks, verbose=args.verbose, text_only=args.text_only)
+    save_outputs(chunks=chunks, verbose=args.verbose, text_only=args.text_only, output=args.output)
